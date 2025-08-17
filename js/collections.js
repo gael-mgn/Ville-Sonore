93719e6eb8548e65a3962b913058dc3c5396b006
gael-mgn <gael.maignan@eivp-paris.fr>
2025-08-17 13:24:08 +0200

    Add files via upload

--- js/collections.js
+++ js/collections.js
@@ -1,7 +1,9 @@
 const collections = [
-  { id: 'enfance', titre: 'Souvenirs d enfance', lien: 'img/enfance.webp' },
+  { id: 'souvenirs-enfance', titre: 'Souvenirs d enfance', lien: 'img/enfance.webp' },
   { id: 'chaleur-urbaine', titre: 'Chaleur urbaine', lien: 'img/chaleur.webp' },
-  { id: 'soir', titre: 'Image 3', lien: 'img/soir.webp' },
+  { id: 'nuit', titre: 'Image 3', lien: 'img/nuit.webp' },
   { id: 'musique-urbaine', titre: 'Image 3', lien: 'img/musique.webp' },
-  { id: 'nuisances-urgences', titre: 'Image 3', lien: 'img/nuisances-urgences.webp' }
+  { id: 'marche', titre: 'Image 3', lien: 'img/marche.webp' },
+  { id: 'nuisances-urgences', titre: 'Image 3', lien: 'img/nuisances-urgences.webp' },
+  { id: 'gare', titre: 'Image 3', lien: 'img/gare.webp' }
 ];
